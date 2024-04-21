25ec7cff75451a8f422dc80b11ff98add2278e09
Javi <jcareagamoraleda@gmail.com>
2024-04-21 19:42:11 +0200

    removed md config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import markdownConfig from './markdown.config';
-import mdx from "@astrojs/mdx";
+import markdownConfig from "./markdown.config";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
@@ -11,36 +10,31 @@ import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.rohitk06.site",
-  prefetch: true,
-  markdown: {
-    ...markdownConfig
-  },
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), react(), mdx({
-    ...markdownConfig,
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "one-dark-pro"
-    }
-  }), sitemap(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), robotsTxt({
-    sitemap: ['https://www.rohitk06.site/sitemap-0.xml', 'http://www.rohitk06.site/sitemap-index.xml']
-  }), playformCompress()]
-  // output: "server",
-  // adapter: vercel({
-  //   webAnalytics: {
-  //     enabled: true
-  //   },
-  //   speedInsights: {
-  //     enabled: true
-  //   },
-  //   imageService: true
-  // })
+	site: "https://www.rohitk06.site",
+	prefetch: true,
+	markdown: {
+		...markdownConfig,
+	},
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		react(),
+
+		sitemap(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+		robotsTxt({
+			sitemap: [
+				"https://www.rohitk06.site/sitemap-0.xml",
+				"http://www.rohitk06.site/sitemap-index.xml",
+			],
+		}),
+		playformCompress(),
+	],
 });
